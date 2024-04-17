77c471f4ca3d22751a6666eaead89041825e2fa3
Wadeed Ur Rahman <155989207+WadeedRahman@users.noreply.github.com>
2024-04-17 17:43:20 +0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,12 +1,35 @@
 from chatterbot import ChatBot
-import spacy
-nlp = spacy.load("en_core_web_sm")
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.logic import BestMatch
+import logging
 
-chatbot = ChatBot("Chatpot")
+# Enable info level logging for the root logger
+logging.basicConfig(level=logging.INFO)
+
+# Filter out INFO messages for the chatterbot logger
+chatterbot_logger = logging.getLogger('chatterbot')
+chatterbot_logger.setLevel(logging.WARNING)
+
+chatbot = ChatBot("Chatpot",
+                  logic_adapters=[
+                      {
+                          'import_path': 'chatterbot.logic.BestMatch',
+                          'default_response': 'I am sorry, but I do not understand,Plz try again.',
+                          'maximum_similarity_threshold': 0.90
+                      }
+                  ]
+)
+
+# Optionally, you can train the chatbot with some data
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train('chatterbot.corpus.custom')
+
 exit_conditions = (":q", "quit", "exit")
+
 while True:
     query = input("Ask Anything:")
-    if query in exit_conditions:
+    if query.lower() in exit_conditions:
         break
     else:
-        print(f"{chatbot.get_response(query)}")
+        response = chatbot.get_response(query)
+        print(response)
